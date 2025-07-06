2beec1478b2f0144b598076bba238163c7d2e1ce
A7medAR <ahmedqatar194@gmail.com>
2025-07-06 16:50:58 +0300

    finished the second refactor my awards screen

--- lib/features/profile/presentation/widgets/my_awards_screen.dart
+++ lib/features/profile/presentation/widgets/my_awards_screen.dart
@@ -32,37 +32,39 @@ class MyAwardsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-        child: ListView.builder(
-          itemCount: rewardImages.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CustomText(
-                    text: "My awards".tr(),
-                    fontWeight: FontWeight.w800,
-                    fontSize: 20.sp,
-                    fontFamily: "Manrope",
-                    color: AppColors.blackDark,
-                  ),
-                  16.ph,
-                  Container(
-                    height: 180,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      image: DecorationImage(
-                        image: AssetImage(rewardImages[index]),
-                        fit: BoxFit.fill,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomText(
+              text: "My awards".tr(),
+              fontWeight: FontWeight.w800,
+              fontSize: 20.sp,
+              fontFamily: "Manrope",
+              color: AppColors.blackDark,
+            ),
+            16.ph,
+            Expanded(
+              child: ListView.builder(
+                itemCount: rewardImages.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Container(
+                      height: 180,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          image: AssetImage(rewardImages[index]),
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  );
+                },
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
